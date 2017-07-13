b986eb462eb634186002b49f7c75cb566b1597b4
NguyenLab <nguyenlab.jaist@gmail.com>
2017-07-13 10:13:44 +0900

    Add files via upload

--- preprocess/preprocess.sh
+++ preprocess/preprocess.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 #-------------------------------------------
+#distribute dirs
+python scripts/distribute-dirs.py data/wiki-javi 10000 data/parts
+
+#-------------------------------------------
 #word segment
 ./scripts/jvntextpro/jvntextpro.sh $PWD/data/.. file_name.vi 15000
 
